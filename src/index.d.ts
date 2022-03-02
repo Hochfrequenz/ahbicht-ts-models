a5b03744a03cbd436f4572d0598ad6ba1cd3ca28
Franziska Vesely <franziska.vesely@hochfrequenz.de>
2022-03-02 10:41:41 +0100

    check if exporting every file would work

--- src/index.d.ts
+++ src/index.d.ts
@@ -1,1 +1,9 @@
-export * from ".";
+export * from "./AhbExpressionEvaluationResultSchema";
+export * from "./CategorizedKeyExtractSchema";
+export * from "./ConditionKeyConditionTextMappingSchema";
+// export * from "./ContentEvaluationResultSchema";
+// export * from "./EvaluatedFormatConstraintSchema";
+// export * from "./FormatConstraintEvaluationResultSchema";
+// export * from "./PackageKeyConditionExpressionMappingSchema";
+// export * from "./RequirementConstraintEvaluationResultSchema";
+export * from "./TokenSchema";
