eceab2e21823ff75ca1a831908610703be0bd212
konstantin <konstantin.klein@hochfrequenz.de>
2022-03-02 19:48:21 +0100

    remove old index.d.ts

--- src/index.d.ts
+++
@@ -1,9 +0,0 @@
-export * from "./AhbExpressionEvaluationResultSchema";
-export * from "./CategorizedKeyExtractSchema";
-export * from "./ConditionKeyConditionTextMappingSchema";
-// export * from "./ContentEvaluationResultSchema";
-// export * from "./EvaluatedFormatConstraintSchema";
-// export * from "./FormatConstraintEvaluationResultSchema";
-// export * from "./PackageKeyConditionExpressionMappingSchema";
-// export * from "./RequirementConstraintEvaluationResultSchema";
-export * from "./TokenSchema";
